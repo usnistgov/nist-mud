346c2fd19b940a9749ca08f38ecbab25e31a95ac
M. Ranganathan <mranga@nist.gov>
2018-05-03 21:53:55 -0400

    Added unit tests.

---
+++ test/unittest/flowmon/postit2.sh
@@ -0,0 +1,1 @@
+curl -v -X PUT -u admin:admin --header "Content-Type:application/json"  --data @../config/ids-config.json  http://$CONTROLLER_ADDR:8181/restconf/config/nist-flowmon-config:flowmon-config
