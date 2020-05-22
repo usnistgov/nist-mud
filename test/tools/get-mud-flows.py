eae6eff8d79455e11b1a1e2e6b7ed2b08ae44465
M. Ranganathan <mranga@nist.gov>
2020-05-21 22:55:51 -0400

    Added some interesting stuff to the end of the script.

---
+++ test/tools/get-mud-flows.py
@@ -0,0 +1,50 @@
+import requests
+import json
+import argparse
+import os
+from os import sys
+import string
+import time
+import ast
+
+def is_part(some_string, target):
+    return target in some_string
+
+if __name__ == "__main__" :
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", help="Metadata/mask in hex with a leading 0x. Cut and paste from dump-flows")
+    args = parser.parse_args()
+    srcmac = args.m
+    if srcmac == None :
+       print("Please provide src mac")
+       sys.exit()
+
+    argmap = {}
+    innerMap = {}
+    innerMap["mud-url"] = srcmac
+    argmap["input"] = innerMap
+    jsonStr = json.dumps(argmap, indent=4)
+    print(jsonStr)
+    url =  "http://127.0.0.1:8181/restconf/operations/sdnmud:add-controller-wait-input"
+    headers= {"Content-Type":"application/json"}
+    while True:
+        r = requests.post(url,headers=headers , auth=('admin', 'admin'), data=jsonStr)
+        if r.content !=  b'{"output":{}}':
+            time.sleep(10)
+            continue
+        else:
+            print("got the mud file - can pull a log")
+            break
+
+    time.sleep(10)
+    # OK if we got so far, we can get the log
+    url = "http://127.0.0.1:8181/restconf/operations/sdnmud:get-mud-reports"
+    r = requests.post(url,headers=headers , auth=('admin', 'admin'),data=jsonStr)
+    while True:
+        r = requests.post(url,headers=headers , auth=('admin', 'admin'), data=jsonStr)
+        js = json.loads(r.content)
+        print("*************************************************")
+        print(js)
+        time.sleep(10)
+        
+
